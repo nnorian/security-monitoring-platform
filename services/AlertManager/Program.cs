4a2111725fcc6b6c041549c72e7619d0fd525c55
nnorian <kushnirenkoecaterina@gmail.com>
2026-02-23 11:53:24 +0200

    feat: intruducend the connection the the postgres db

--- services/AlertManager/Program.cs
+++ services/AlertManager/Program.cs
@@ -1,1 +1,5 @@
 builder.Services.AddHostService<LogConsumerService>();
+
+builder.Services.AddDbContext<AlbertDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Postgres")
+        ?? $"Host={builder.Configuration["POSTGRES_HOST"]};Database=security_events;Username={builder.Configuration["POSTGRES_USER"]};Password={builder.Configuration["POSTGRES_PASSWORD"]}"));

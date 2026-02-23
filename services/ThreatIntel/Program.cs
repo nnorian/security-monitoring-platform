4cbf37f4274ee8c291a3f271352095afb8eea2ea
nnorian <kushnirenkoecaterina@gmail.com>
2026-02-23 20:40:22 +0200

    feat: added redis caching to the threat inteligence service

--- services/ThreatIntel/Program.cs
+++ services/ThreatIntel/Program.cs
@@ -1,4 +1,25 @@
 builder.Service.AddMetrics();
 
 app.UseHttpMetrics();
-app.MapMetrics();
+app.MapMetrics(); 
+
+//exposes /metrics endpoint
+builder.Services.AddSingleton<IConnectionMultiplexer>(ConnectionMultiplexer.Connect(builder.Configuration["REDIS_HOST"] ?? "localhost"));
+
+app.MapGet("/threat/check/{ip}", async (string ip, IConnectionMultiplex redis, HttpClient http) =>{
+    var db = redis.GetDatabase();
+    var cacheKey = $"threat:{ip}";
+
+    var cached = await db.StringGetAsync(cacheKey);
+    if (cached.HasValue)
+        return Result.Ok(new { ip, fromCache = true, data = cached.ToString() });
+    
+    var response = await http.GetStringAsync(
+        $"https://api.abuseipbd.com/api/v2/check?ipAddress={ip}"
+    );
+
+    //cache for one hour
+    await db.stringAsync(cacheKey, response, TimeSpan.FromHours(1));
+
+    return Results.Ok(new {ip, fromCache = false, data = response});
+})
